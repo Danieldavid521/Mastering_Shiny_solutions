036d685402ac17252958831d07c29e2259b94641
DanielDavid521 <15642147+Danieldavid521@users.noreply.github.com>
2020-01-14 10:19:47 -0800

    Update MS_2-2_exercise.R

--- MS_2-2_exercise.R
+++ MS_2-2_exercise.R
@@ -1,11 +1,5 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#excerise solutions for the book https://mastering-shiny.org/
+
 
 library(shiny)
 
@@ -17,7 +11,7 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
     output$num2 <- renderText({
-      paste0("Your selected input * 5 is", input$num2, "!")
+        paste0("Your selected input * 5 is ", input$num2 * 5, "!")
     })
 }
 
