8c07c862b2702773429b8b88483a0ad41078dbca
DanielDavid521 <15642147+Danieldavid521@users.noreply.github.com>
2020-01-18 10:35:06 -0600

    Create Mastering_shiny_Chapter_3-2-8-1_exercise_solution.R

---
+++ Mastering_shiny_Chapter_3-2-8-1_exercise_solution.R
@@ -0,0 +1,13 @@
+library(shiny)
+
+
+
+ui <- fluidPage(
+    sliderInput("input_date", "Select Date", min= as.Date("2015-01-01"), max = as.Date("2015-07-01"), value = as.Date("2015-04-01"), timeFormat = "%F"),
+    
+)
+
+server <- function(input, output, session) {
+    
+}
+shinyApp(ui=ui, server=server)
